c7970c17c66f674bd5e48152fab051b2362ab103
Abdul Hanan <abdulhana03@gmail.com>
2024-07-31 07:52:11 +0500

    Solution of JS FILES

---
+++ script.js
@@ -0,0 +1,7 @@
+// const parent= document.querySelector(`.parent`)
+
+// // parent.firstElementChild.style.color="orange";
+// // also we can get lastelementof parent
+// const childone=parent.firstElementChild;
+
+// childone.parentElement.style.color="grey";
