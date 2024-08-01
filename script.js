99c4cbcc435660ab993d6bbf089c51b6057999a0
Abdul Hanan <abdulhana03@gmail.com>
2024-08-01 12:35:08 +0500

    JS_Mastry

--- script.js
+++
@@ -1,7 +0,0 @@
-// const parent= document.querySelector(`.parent`)
-
-// // parent.firstElementChild.style.color="orange";
-// // also we can get lastelementof parent
-// const childone=parent.firstElementChild;
-
-// childone.parentElement.style.color="grey";
