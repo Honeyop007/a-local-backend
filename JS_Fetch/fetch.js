201a066f3e797f4082d52149ec080b8649af4b45
Abdul Hanan <abdulhana03@gmail.com>
2024-08-01 14:39:24 +0500

    Fetch js

---
+++ JS_Fetch/fetch.js
@@ -0,0 +1,25 @@
+const user = async ()=> {
+try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+ console.log(data);
+} catch (error) {
+    console.log('E:', error);
+}
+}
+user();
+
+fetch('https://jsonplaceholder.typicode.com/users').then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    console.log(data);
+    return data.name;
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    console.log("E:",err);
+})
+
